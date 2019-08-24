d07d0131c528aac118f6b81c937ec79e4c106a92
Egkurilov <Egkurilov@gmail.com>
2019-08-24 14:44:25 +0300

    reformat file add comment

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from time import sleep
 
+# список рейдовых дней
 raid_days = {
     'Monday': '21:21',
     'Tuesday': ['21:07', '21:14', '21:21'],
@@ -11,18 +12,34 @@ raid_days = {
     'Sunday': '19:21'}
 
 
+# получаем день недели
 def get_day_of_weak():
     now_dow = datetime.now().strftime("%A")
     return now_dow
 
 
-def get_event_time():
+# получаем текущее время
+def get_time_now():
     now_time = datetime.now().strftime("%H:%M")
     return now_time
 
 
-while True:
-    if get_day_of_weak() in raid_days:
-        print(get_day_of_weak(), raid_days[get_day_of_weak()])
+# удаленный ивент  todo
+def ts_event(time):
+    print(time)
 
+
+while True:
+    # проверяем день в списке рейдовых дней
+    if get_day_of_weak() in raid_days:
+        # принт для дебага
+        print(get_day_of_weak(), raid_days[get_day_of_weak()])
+        # считываем список в строку для проверки
+        for time in raid_days[get_day_of_weak()]:
+            # сравниваем текущее время и время рейдов
+            if get_time_now == time:
+                print(ts_event, "время очередного события")
+            else:
+                print("Время еще не пришло")
+    # время проверки
     sleep(60)
